0de3a02d5be103fc0e5381e3378acfd56e70bcc4
champy0527 <138946854+champy0527@users.noreply.github.com>
2024-07-23 01:03:35 +0100

    Explore button vs canvas option for right and wrong image

--- main.py
+++ main.py
@@ -7,24 +7,28 @@ from random import choice
 BACKGROUND_COLOR = "#B1DDC6"
 LANGUAGE_FONT = ("Arial", 40, "italic")
 WORD_FONT = ("Arial", 60, "bold")
+word_library_df = pd.read_csv("data/french_words.csv")
 
+# -------------------------- LANGUAGES ------------------------------ #
+
+word_library_df.columns = list(word_library_df)  # to put header inside data frame
+language = list(word_library_df.columns)
+print(language)
 
 # ---------------------------- WORDS ------------------------------- #
 
-word_library = pd.read_csv("data/french_words.csv")
-frequency_dictionary = {row["French"]: row["English"] for (index, row) in word_library.iterrows()}
+
+frequency_dictionary = {row["French"]: row["English"] for (index, row) in word_library_df.iterrows()}
 # frequency_dictionary = pd.DataFrame.to_dict(word_library)
 
 
-print(frequency_dictionary)
+print(frequency_dictionary)  # debugger
 
 
 def pick_random_word():
     french_word = choice(list(frequency_dictionary.keys()))
     canvas.itemconfig(word_text, text=f"{french_word}")
 
-
-# print(pick_random_word())
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -48,10 +52,16 @@ wrong_img = PhotoImage(file="./images/wrong.png")
 wrong_img_item = canvas.create_image(200, 576, image=wrong_img)
 canvas.tag_bind(wrong_img_item, '<Button-1>', lambda event: pick_random_word())
 canvas.grid(column=0, row=1)
+# wrong_button = Button(image=wrong_img, highlightthickness=0, borderwidth=0, command=pick_random_word)
+# wrong_button.grid(column=0, row=1)
+
 
 right_img = PhotoImage(file="./images/right.png")
 right_img_item = canvas.create_image(600, 576, image=right_img)
 canvas.grid(column=1, row=1)
+# right_button = Button(image=right_img, highlightthickness=0, borderwidth=0, )
+# right_button.grid(column=1, row=1)
 
+pick_random_word()
 
 window.mainloop()
